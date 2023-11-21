d13751ab4d4b0eaa12378ea0f5d306d81c8e966c
emsenn <emsenn@emsenn.net>
2023-11-21 15:00:44 +0000

    chg: Improve thing.fullName() and thing.describe()

--- behaviors/thing.js
+++ behaviors/thing.js
@@ -1,14 +1,22 @@
 function fullName() {
-  const name = (this.name || "some thing");
+  let name = '';
+  if (this.name) {
+    if (this.grammar && this.grammar.article) {
+      name += (this.grammar.article.length === 0 ? '' : this.grammar.article) + ' ' + this.name;
+    } else {
+      name += (['a', 'e', 'i', 'o', 'u'].includes(this.name[0].toLowerCase()) ? "an " : "a ") + this.name;
+    }
+  } else {
+    name = "thing";
+  }
   return name;
 }
 
 function describe() {
-  const description = (this.description || "This is " + this.fullName());
-  return description;
+  return this.description ? this.description : "This is " + this.fullName() + ".";
 }
 
 module.exports = {
   fullName,
-  describe,
+  describe
 }
